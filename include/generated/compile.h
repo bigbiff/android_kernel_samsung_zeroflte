bfede74ff17ee0e09f941aff5e23b69da289b044
bigbiff <bigbiff@build.teamw.in>
2015-04-13 21:21:43 -0400

    initial import

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Apr 11 14:56:22 EDT 2015"
+#define LINUX_COMPILE_BY "jenkins"
+#define LINUX_COMPILE_HOST "build"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
